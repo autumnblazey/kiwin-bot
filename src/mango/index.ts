ad801f9d25681c411c8671159f6e22e42e7414c9
Alexander Liu <liujalexander84@gmail.com>
2021-03-10 21:06:45 -0800

    add a db

---
+++ src/mango/index.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+export function dbclient() {
+   const prisma = new PrismaClient();
+
+   return {
+      async getprefix(id: string): Promise<string> {
+         const found = await prisma.server.findUnique({
+            where: { id },
+            rejectOnNotFound: false,
+            select: { prefix: true }
+         });
+
+         if (found) return found.prefix;
+
+         const created = await prisma.server.create({
+            data: { id }
+         });
+
+         return created.prefix;
+      }
+      // async setprefix(id: string, prefix: string): Promise<void> {}
+   };
+}
